58c593662138079daa5adc0fd45b062cf7510d50
luvhirani <luvhirani0@gmail.com>
2025-08-07 14:31:12 +0530

    mongo

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,13 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try {
-        mongoose.connection.on('connected', ()=> console.log("Database Connected")
-        );
-        await mongoose.connect(`${process.env.MONGODB_URI}`)
-    } catch (error) {
-        console.error(error.message);
-    }
+        await mongoose.connect(process.env.MONGODB_URI);
+    
+        console.log("✅ Database Connected");
+    
+        mongoose.connection.on("disconnected", () => {
+          console.log("MongoDB disconnected");
+        });
+    
+      } catch (error) {
+        console.error("MongoDB connection error:", error.message);
+      }
 }
-
 
 export default connectDB;
