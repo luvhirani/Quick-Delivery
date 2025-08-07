cad69b6afa393be11e187da69fa0be0ad2ccc9fc
luvhirani <luvhirani0@gmail.com>
2025-08-07 15:16:42 +0530

    test

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,13 +2,17 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
-    
-        console.log("✅ Database Connected");
-    
-        mongoose.connection.on("disconnected", () => {
-          console.log("MongoDB disconnected");
-        });
+        const client = new mongoose(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        client.connect()
+            .then(() => {
+                console.log('start');
+            })
+            .catch(err => {
+                console.error('App starting error:', err.stack);
+                process.exit(1)
+            });
+        
     
       } catch (error) {
         console.error("MongoDB connection error:", error.message);
